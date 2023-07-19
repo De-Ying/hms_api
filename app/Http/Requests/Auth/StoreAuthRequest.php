ea00ede5b9ca30c6c101266053c8eeed0d52da9a
De-Ying-Course <ducanh25820@gmail.com>
2023-07-19 16:20:06 +0700

    Update StoreAuthRequest

--- app/Http/Requests/Auth/StoreAuthRequest.php
+++ app/Http/Requests/Auth/StoreAuthRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\FileTypeValidate;
+use Illuminate\Validation\Rules\Password;
 
 class StoreAuthRequest extends FormRequest
 {
